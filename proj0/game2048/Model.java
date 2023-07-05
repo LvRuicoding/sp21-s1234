e6b170cc49ae21d5ba00737eebb016b756e2466a
LvRuicode <LvRuicoding>
2023-07-05 09:04:25 +0800

    Done with some methods

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -110,6 +110,7 @@ public class Model extends Observable {
         boolean changed;
         changed = false;
 
+
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
@@ -138,6 +139,12 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if(b.tile(i,j)==null){return true;}
+            }
+
+        }
         return false;
     }
 
@@ -148,6 +155,14 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if(b.tile(i,j)!=null){
+                    if(b.tile(i,j).value()==MAX_PIECE){return true;}
+                }
+            }
+
+        }
         return false;
     }
 
@@ -159,6 +174,20 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        // Empty check
+        if(Model.emptySpaceExists(b)){return true;}
+        //Row check
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                if((b.tile(i,j).value()==b.tile(i,j+1).value())){return true;}
+            }
+        }
+        //Column ckeck
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                if((b.tile(j,i).value()==b.tile(j+1,i).value())){return true;}
+            }
+        }
         return false;
     }
 
